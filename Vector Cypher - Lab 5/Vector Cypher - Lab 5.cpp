34efbbc65cfd5618b887f30b27357c0c3eb521b8
abounnaaim-a <aabounna@purdue.edu>
2023-12-16 20:22:36 -0500

    CGT 215

--- Vector Cypher - Lab 5/Vector Cypher - Lab 5.cpp
+++ Vector Cypher - Lab 5/Vector Cypher - Lab 5.cpp
@@ -2,19 +2,33 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+std::vector<char> cypher = { 'V','F','X','B','L','I','T','Z','J','R','P','H',
+'D','K','N','O','W','S','G','U','Y','Q','M','A','C','E' };
+
+char translate(char c) {
+	if (c >= 65 && c <= 90) {
+		return cypher[c - 65];
+	}
+    else if (c >= 97 && c <= 122) {
+        char upperCaseLetter = c - 32;
+        char upperCaseCode = cypher[upperCaseLetter - 65];
+        return upperCaseCode + 32;
+    }
+    else {
+        return c;
+    }
 }
+int main() {
+    std::string text;
+    std::cout << "Input text to cypher: ";
+    getline(std::cin, text);
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    for (char& c : text) {
+        c = translate(c);
+    }
+    std::cout << "Encodes Message: " << text << "..." << std::endl;
+    return 0;
+}
